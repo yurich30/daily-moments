6b60c049d14645e08615e392646eadbba5477294
Yurii <yurich3098@gmail.com>
2021-06-27 19:31:50 +0300

    add firebase to project

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAMLKbxVYpE0h6Xz8z49beAZhiyjv8FdUE",
+    authDomain: "daily-moments-d4917.firebaseapp.com",
+    projectId: "daily-moments-d4917",
+    storageBucket: "daily-moments-d4917.appspot.com",
+    messagingSenderId: "319408579531",
+    appId: "1:319408579531:web:efff6210e42b5407488c4e",
+    measurementId: "G-BMEFKL985H"
+  }
+
+firebase.initializeApp(firebaseConfig)
